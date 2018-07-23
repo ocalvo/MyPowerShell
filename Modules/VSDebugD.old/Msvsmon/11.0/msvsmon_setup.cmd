fe048ad552c2b347afa2abdccc2dd8cc941235b2
Oscar Calvo <{ID?}-{username}@users.noreply.github.com>
2018-07-23 12:38:01 -0700

    Remove old files

--- Modules/VSDebugD.old/Msvsmon/11.0/msvsmon_setup.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-reg add "HKLM\Software\Microsoft\.NetFramework" /f /v COMPLUS_DbgUseTransport  /t REG_DWORD /d 0
-reg add "HKLM\Software\Microsoft\.NetFramework" /f /v DbgUseTransport /t REG_DWORD /d 0
-reg add "HKLM\Software\Microsoft\.NetFramework" /f /v DbgPackShimPath /t REG_SZ /d C:\windows\system32\dbgshim.dll
-reg add "HKLM\Software\Microsoft\VisualStudio\Debugger" /f /v EmulateExclusiveBreakpoints /t REG_DWORD /d 1
-reg add "HKLM\Software\Microsoft\VisualStudio\11.0\Debugger" /f /v EngineEnableEnumeration  /t REG_DWORD /d 1
