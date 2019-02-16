7e7ff48fde5e61ff47b72e3b191f2d3286a3989b
Oscar Calvo <oscar@calvonet.com>
2019-02-16 13:03:31 -0800

    Delete crap

--- Connect-OneDrive.ps1
+++ Connect-OneDrive.ps1
@@ -5,9 +5,6 @@ param(
 $lid = "DA19DC9EC43EAA62"
 $skyDrive = ('https://d.docs.live.net/'+$lid+'/')
 $user = 'oscar@calvonet.com'
-$pass = 'Cubujuqui5729'
 
-echo $skyDrive
-net use s: $skyDrive $pass  /user:$user /persistent:no
-robocopy s:\ $rootDir /E
-#net use s: /delete
+Write-Output $skyDrive
+net use u: $skyDrive /user:$user /persistent:yes
