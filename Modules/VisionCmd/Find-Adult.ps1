345180f258c478590cb4089e4c078d784e528f72
Oscar Calvo <oscar@calvonet.com>
2019-02-16 13:31:54 -0800

    Initial commit for VisionCmd

---
+++ Modules/VisionCmd/Find-Adult.ps1
@@ -0,0 +1,20 @@
+﻿set-alias vcmd C:\dd\Repos\CS\MediaSync\Exts\Vision\Cmd\bin\Debug\VisionCmd.exe
+
+dir -dir -rec |% {
+  echo ("Looking for adult content in "+$_.FullName+"...")
+  $marker = ($_.FullName+"\.adult")
+  if (!(test-path $marker))
+  {
+    $adult = dir -path $_.FullName -file *.jpg | where { echo $_.FullName;vcmd $_.FullName } | select -first 1
+    if ( $adult -ne $null )
+    {
+      echo ".adult" > $marker
+      echo ("Found new adult content: "+$_.FullName)
+    }
+    else
+    {
+      echo ".noadult" > $marker
+      echo ("Found G content: "+$_.FullName)
+    }
+  }
+}
