3666f20e45a5202e0683cfbb1f3aa1cae55d0003
Oscar Calvo <oscar@calvonet.com>
2021-05-30 19:39:05 -0700

    Update

--- _setup.ps1
+++ _setup.ps1
@@ -1,3 +1,10 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n allowGlobalConfirmation
 choco install git
+$env:path+=(";"+"C:\Program Files\Git\bin\")
+git clone https://github.com/ocalvo/MyPowerShell.git ($env:HOMEDRIVE+$env:HOMEPATH+"\Documents\WindowsPowerShell")
+new-Item ~\Documents\PowerShell -ItemType SymbolicLink -Target ~\Documents\WindowsPowerShell
+
+choco install cacadiafonts
+
+.$profile
