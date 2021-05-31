4f21e1a98cda54ca9ad726d5e939230d84f6cae7
Oscar Calvo <ocalvo@microsoft.com>
2021-05-31 14:28:12 -0700

    Configure sshd

--- _setup.ps1
+++ _setup.ps1
@@ -5,6 +5,23 @@ Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 # Install the OpenSSH Server
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
+# Start the sshd service
+Start-Service sshd
+#
+# OPTIONAL but recommended:
+Set-Service -Name sshd -StartupType 'Automatic'
+#
+# Confirm the firewall rule is configured. It should be created automatically by setup.
+Get-NetFirewallRule -Name *ssh*
+#
+# There should be a firewall rule named "OpenSSH-Server-In-TCP", which should be enabled
+# If the firewall does not exist, create one
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+
+New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+
+powershell -nologo -noprofile -command Set-ExecutionPolicy bypass -force
+
 if ($null -eq (get-command choco*))
 {
   iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
