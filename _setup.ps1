30e2e27f6d4ff28247d83a0159e081ee1463eba1
Oscar Calvo <ocalvo@microsoft.com>
2021-05-30 19:50:48 -0700

    Update

--- _setup.ps1
+++ _setup.ps1
@@ -2,10 +2,17 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 choco feature enable -n allowGlobalConfirmation
 choco install git
 $env:path+=(";"+"C:\Program Files\Git\bin\")
-git clone https://github.com/ocalvo/MyPowerShell.git ($env:HOMEDRIVE+$env:HOMEPATH+"\Documents\WindowsPowerShell")
+$_psD = ($env:HOMEDRIVE+$env:HOMEPATH+"\Documents\WindowsPowerShell")
+git clone https://github.com/ocalvo/MyPowerShell.git $_psD
 new-Item ~\Documents\PowerShell -ItemType SymbolicLink -Target ~\Documents\WindowsPowerShell
+
+cd $_psD
+git submodule update --init .\vimfiles\
 
 choco install cascadiafonts
 choco install pwsh
 
 .$profile
+
+.\Set-GitConfig.ps1
+
