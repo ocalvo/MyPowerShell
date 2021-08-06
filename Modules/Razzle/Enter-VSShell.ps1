753285da4d1a9f725477b80e465e194ac09d2f3f
Oscar Calvo <ocalvo@microsoft.com>
2021-08-06 11:08:18 -0700

    Update Enter-VsShell for VS 2022

--- Modules/Razzle/Enter-VSShell.ps1
+++ Modules/Razzle/Enter-VSShell.ps1
@@ -1,19 +1,35 @@
 param(
-  [Parameter(Mandatory=$false)][String]$vsVersion = "Preview")
+  [Parameter(Mandatory=$false)][String]$vsVersion = "Preview",
+  [Parameter(Mandatory=$false)][switch]$x64 = $false
+)
 
 $installPath = &"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version 16.0 -prerelease -property installationpath
 $vsVersions = $installPath |
   Select-Object @{Name='Version';Expression={Split-Path $_ -Leaf | Select-Object -First 1}},
     @{Name='Path';Expression={$_}}
 
-$ver = $vsVersions | Where-Object {$_.Version -eq $vsVersion }
+$bitness = "*86*"
+if ($x64.IsPresent)
+{
+  $bitness = ""
+}
+$startPath = (get-item ("env:ProgramFiles"+$bitness)).Value
+$ver = $vsVersions | Where-Object {($_.Version -eq $vsVersion) -and ($_.Path.StartsWith($startPath)) }
+Write-Host "Found the following versions:"
+$ver |% {
+  Write-Host ("  "+$_)
+}
+
 if ($null -eq $ver)
 {
   throw "Visual Studio version $vsVersion not found"
 }
 $devShellModule = Join-Path $ver.Path "Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+Write-Host "Loading module $devShellModule"
 Import-Module $devShellModule
-Enter-VsDevShell -VsInstallPath $ver.Path -SkipAutomaticLocation
+$vsVerPath = $ver.Path
+Write-Host "Loading VS Shell from $vsVerPath"
+Enter-VsDevShell -VsInstallPath $vsVerPath -SkipAutomaticLocation
 
 .$PSScriptRoot\MSBuild-Alias.ps1
 
