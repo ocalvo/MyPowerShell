53435981bd53bb93e448107b8417cc44228e45b7
Oscar Calvo <ocalvo@microsoft.com>
2024-03-29 11:33:46 -0700

    Add trace_timebuild.ps1

---
+++ trace_timebuild.ps1
@@ -0,0 +1,47 @@
+param(
+  $taskName,
+  $taskFile)
+
+$global:tasks = [xml](cat $taskFile)
+
+function Get-Task {
+  param ($t)
+
+  $tasks.Tasks.Task | where { $_.Name -eq $t }
+}
+
+function Get-TaskDependencies {
+  param($task, $level=0)
+
+  $n = $task.Name
+  $blankString = [string]::Empty.PadLeft($level, ' ')
+
+  $isLeaf = $true
+  $task.Dependency |% {
+    $subTask = Get-Task $_.Name
+    if ($null -eq $subTask) {
+      return
+    }
+    $isLeaf = $false
+    $n = $subTask.Name
+    Get-TaskDependencies -task $subTask -level ($level+1)
+  }
+
+  if ($isLeaf) {
+    Write-Host "$blankString$n"
+    return $task
+  }
+
+  return $null
+}
+
+$task = (Get-Task $taskName)
+if ($null -eq $task) {
+  Write-Error "Task $taskName not found"
+  return
+}
+$n = $task.Name
+Write "Processing dependencies for $n"
+
+Get-TaskDependencies $task
+
