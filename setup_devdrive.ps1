6c35d20096e4f5534be1060dd4da5a768b200bff
Oscar Calvo <ocalvo@microsoft.com>
2024-06-28 14:42:05 -0700

    Remove not needed files

---
+++ setup_devdrive.ps1
@@ -0,0 +1,25 @@
+#Requires -RunAsAdministrator
+[CmdletBinding()]
+param (
+  $srcDrive = "f:",
+  $binDrive = "w:"
+)
+
+#Format-Volume -DriveLetter $binDrive -DevDrive
+#Format-Volume -DriveLetter $srcDrive -DevDrive
+
+setx /M _NT_SYMBOL_PATH "SRV*$binDrive\symbols*http://symweb"
+setx /M npm_config_cache "$srcDrive\packages\npm"
+setx /M NUGET_PACKAGES "$srcDrive\packages\nuget"
+setx /M VCPKG_DEFAULT_BINARY_CACHE "$srcDrive\packages\vcpkg"
+setx /M PIP_CACHE_DIR "$srcDrive\packages\pip"
+setx /M CARGO_HOME "$srcDrive\packages\cargo"
+setx /M MAVEN_OPTS "-Dmaven.repo.local=$srcDrive\packages\maven %MAVEN_OPTS%"
+setx /M GRADLE_USER_HOME "$srcDrive\packages\gradle"
+setx /M MSRUSTUP_HOME "$srcDrive\msrust"
+
+fsutil devdrv trust $binDrive
+fsutil devdrv trust $srcDrive
+fsutil devdrv enable /disallowAv
+fsutil devdrv setfiltersallowed "PrjFlt, DfmFlt"
+
