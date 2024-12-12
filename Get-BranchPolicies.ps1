d25a12c2cea11efdd0475feec15178d9d841fa4e
Oscar Calvo <ocalvo@microsoft.com>
2024-12-11 18:04:23 -0800

    Add Get-BranchPolicies.ps1

--- Get-BranchPolicies.ps1
+++ Get-BranchPolicies.ps1
@@ -3,7 +3,8 @@ param(
   $organization = "Microsoft",
   $project = "OSGTools",
   $repositoryName = "ES.Build.Rings.Configuration",
-  $branchName = "main",
+  $branchName = "refs/heads/main",
+  $policyType = "pullRequest",
   $azureDevopsResourceId = "499b84ac-1321-427f-aa17-267ca6975798")
 
 Write-Verbose "repositoryName: $repositoryName"
@@ -23,22 +24,23 @@ Write-Verbose "repositoryName: $repositoryName"
 # Get the repository ID
 $url = "https://dev.azure.com/${organization}/${project}/_apis/git/repositories/${repositoryName}?api-version=6.0"
 Write-Verbose "$url"
-$repositoryResponse = Invoke-RestMethod -Uri $url -Method Get -Headers $headers
-
-$repositoryResponse.Content | ConvertTo-Json
-
+$global:repositoryResponse = Invoke-RestMethod -Uri $url -Method Get -Headers $headers
 $repositoryId = $repositoryResponse.id
 
 Write-Verbose "repositoryId: $repositoryId"
 
 # Get the branch policies
-$url = "https://dev.azure.com/${organization}/${project}/_apis/policy/configurations?repositoryId=${repositoryId}&refName=${branchName}&api-version=6.0"
+$url = "https://dev.azure.com/${organization}/${project}/_apis/policy/configurations?repositoryId=${repositoryId}&refName=${branchName}&policyType=${$policyType}&api-version=6.0"
 Write-Verbose "$url"
 $policiesResponse = Invoke-RestMethod -Uri $url -Method Get -Headers $headers
 
 # Extract policy IDs
 $policyIds = $policiesResponse.value | ForEach-Object { $_ }
 
-# Output the policy IDs
-$policyIds
+$policyIds = $policyIds | where {
+  $_.settings.scope.refName -eq $branchName -and $_.settings.scope.repositoryId -eq $repositoryId -and $_.isEnabled -and $_.isBlocking -and (-not $_.isDeleted) }
+
+# $policyIds
+
+$policyIds | Select-Object @{Name="Id";Expression={$_.Id}},@{Name="Type";Expression={$_.type.id}},@{Name="Name";Expression={$_.type.displayName}}
 
