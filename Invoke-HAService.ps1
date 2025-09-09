b8811e27db2e62bab3243704b5dae76dadc3a1ef
Oscar Calvo <oscar@calvonet.com>
2025-09-08 18:31:10 -0700

    Update HA service

--- Invoke-HAService.ps1
+++ Invoke-HAService.ps1
@@ -1,6 +1,6 @@
 [CmdLetBinding()]
 param(
-  $device = "den_lights",
+  $device = "den_bookshelve",
   $http = "https",
   $hahost = "homeassistant.local",
   $haport = 8123,
@@ -13,11 +13,15 @@ param(
 
 $token = Get-content '~\Documents\Passwords\wa.hass.txt'
 $method = "api/services/$devicedomain/$action"
-$headers = @{"Authorization"="Bearer $token"}
+$headers = @{
+ "Authorization"="Bearer $token";
+  "content-type"="application/json"}
+Write-Verbose ("Header:`n{0}" -f ($headers | ConvertTo-Json))
 $body = @{"entity_id"="$devicefullname"}
 
 $jsonBody = ( $body | ConvertTo-Json )
-Write-Verbose "Invoking $base_url/$method with body: $jsonBody"
+Write-Verbose "Invoking $base_url/$method with body:`n$jsonBody"
+
 $status = Invoke-WebRequest -Method Post "$base_url/$method" -Headers $headers -Body $jsonBody -SkipCertificateCheck
 if ("OK" -ne $status.StatusDescription) {
   Write-Error $status
